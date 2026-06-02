2ba271e2a7fa791061e47b1abbb0d5ce0577d38a
akbaraminjonov2-tech <akbaraminjonov2@gmail.com>
2026-06-02 16:52:24 +0500

    ifelse02.py bajarildi

--- ifelse02.py
+++ ifelse02.py
@@ -1,1 +1,21 @@
+a = float(input("1-son: "))
+b = float(input("2-son: "))
+amal = input("Amal: ")
 
+if amal == "+":
+    print(f"Natija: {a} + {b} = {a + b}")
+
+if amal == "-":
+    print(f"Natija: {a} - {b} = {a - b}")
+
+if amal == "*":
+    print(f"Natija: {a} * {b} = {a * b}")
+
+if amal == "/":
+    if b == 0:
+        print("Nolga bo'lish mumkin emas!")
+    else:
+        print(f"Natija: {a} / {b} = {a / b}")
+
+if amal != "+" and amal != "-" and amal != "*" and amal != "/":
+    print("Noto'g'ri amal. Faqat +, -, *, / ishlatiladi.")
