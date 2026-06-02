f1f575f11a499be95b4d8b0375bb93cb0d1f9a78
akbaraminjonov2-tech <akbaraminjonov2@gmail.com>
2026-06-02 16:47:33 +0500

    ifelse01.py bajarildi

--- ifelse01.py
+++ ifelse01.py
@@ -1,1 +1,12 @@
+exam_score = int(input("Exam score: "))
 
+if 90 <= exam_score <= 100:    
+    print("A(A'lo)")
+elif 80 <= exam_score <= 89:    
+    print("B(Yaxshi)")
+elif 70 <= exam_score <= 79:    
+    print("C(Qoniqarli")
+elif 60 <= exam_score <= 69:    
+    print("D(Qoniqarsiz)")
+else:
+    print("F(Rad)")
