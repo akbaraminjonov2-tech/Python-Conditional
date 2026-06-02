afaae04011201534ffbd04201d86d67de484372c
akbaraminjonov2-tech <akbaraminjonov2@gmail.com>
2026-06-02 16:26:02 +0500

    if02.py bajarildi

--- if02.py
+++ if02.py
@@ -1,1 +1,15 @@
+parol = input("Parol: ")
 
+harf = False
+raqam = False
+
+for belgi in parol:
+    if belgi.isalpha():
+        harf = True
+    if belgi.isdigit():
+        raqam = True
+
+if len(parol) >= 8 and harf and raqam:
+    print("Parol qabul qilindi.")
+else:
+    print("Parol noto'g'ri. Kamida 8 belgi, 1 harf va 1 raqam bo'lishi kerak.")
