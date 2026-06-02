04f29e005450f37b6e321da65fa8b750ee4c8ff0
akbaraminjonov2-tech <akbaraminjonov2@gmail.com>
2026-06-02 16:03:27 +0500

    elif04.py bajarildi

--- elif04.py
+++ elif04.py
@@ -1,1 +1,17 @@
+narx = 100
+yosh = int(input("Yosh: "))
 
+if yosh < 7:
+    narx = 50
+    print("Yakuniy narx: 50 so'm (50% chegirma qo'llanildi)")
+
+if yosh >= 7 and yosh <= 17:
+    narx = 80
+    print("Yakuniy narx: 80 so'm (20% chegirma qo'llanildi)")
+
+if yosh > 60:
+    narx = 70
+    print("Yakuniy narx: 70 so'm (30% chegirma qo'llanildi)")
+
+if yosh >= 18 and yosh <= 60:
+    print("Yakuniy narx: 100 so'm")
