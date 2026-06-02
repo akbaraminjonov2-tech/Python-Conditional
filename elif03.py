20302a93f0c8e29de6167947662e72de23551483
akbaraminjonov2-tech <akbaraminjonov2@gmail.com>
2026-06-02 15:56:41 +0500

    elif03.py bajarildi

--- elif03.py
+++ elif03.py
@@ -1,1 +1,12 @@
+template = "{} soni {} ga bo'linadi"
 
+n = int(input("Son: "))
+ 
+if n % 2 == 0:
+    print(template.format(n, 2))
+
+if n % 3 == 0:
+    print(template.format(n, 3))
+
+if n % 5 == 0:
+    print(template.format(n, 5))    
