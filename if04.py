bc1c20bcfa7b1b1193f91f148b6fbd54c9ebdf84
akbaraminjonov2-tech <akbaraminjonov2@gmail.com>
2026-06-02 16:36:15 +0500

    if04.py bajarildi

--- if04.py
+++ if04.py
@@ -1,1 +1,11 @@
+balans = 5000
+summa = int(input("Summa: "))
 
+if summa < 0:
+    print("Manfiy summa kiritib bo'lmaydi.")
+
+elif summa <= balans:
+    print(f"Pul yechildi. Qolgan balans: {balans - summa} so'm")
+
+else:
+    print("Mablag' yetarli emas. Sizning balansingiz: 5000 so'm")
