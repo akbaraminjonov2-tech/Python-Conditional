6c5be6d4ac56676e28e4c3fb2fc18112c580540e
akbaraminjonov2-tech <akbaraminjonov2@gmail.com>
2026-06-03 12:15:09 +0500

    ifelse04.py bajarildi

--- ifelse04.py
+++ ifelse04.py
@@ -1,1 +1,16 @@
+masofa = float(input("Masoafa: "))
 
+if masofa < 0:
+    print("Masofa manfiy bo'la olmaydi!")
+
+if masofa >= 0 and masofa <= 1:
+    print("Piyoda yuring")
+
+if masofa > 1 and masofa <= 5:
+    print("Velosiped yoki elektr skuter")
+
+if masofa > 5 and masofa <= 50:
+    print("Avtobus yoki mashina")
+
+if masofa > 50:
+    print("Poyezd yoki samolyot")
