c31ff6272f88c93e6641bc665b810256a71f00df
akbaraminjonov2-tech <akbaraminjonov2@gmail.com>
2026-06-03 13:25:30 +0500

    ifelse05.py bajarildi

--- ifelse05.py
+++ ifelse05.py
@@ -1,1 +1,16 @@
+vazn = float(input("Vazn (kg): "))
+boy = float(input("Bo'y (m): "))
 
+bmi = vazn / (boy ** 2)
+
+if bmi < 18.5:
+    print("Kam vazn")
+
+if bmi >= 18.5 and bmi < 25:
+    print("Normal vazn")
+
+if bmi >= 25 and bmi < 30:
+    print("Ortiqcha vazn")
+
+if bmi >= 30:
+    print("Semizlik")
